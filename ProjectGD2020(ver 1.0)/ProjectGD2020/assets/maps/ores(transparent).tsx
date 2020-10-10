fcdb0248514269d1fe85ad41d71f61bc65164ac7
waliyismail <41659566+waliyismail@users.noreply.github.com>
2020-10-10 10:57:42 +0800

    added visual studio file

---
+++ ProjectGD2020(ver 1.0)/ProjectGD2020/assets/maps/ores(transparent).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="ores(transparent)" tilewidth="16" tileheight="16" tilecount="14" columns="7">
+ <image source="../ores(transparent).png" trans="ff00ff" width="112" height="32"/>
+</tileset>
