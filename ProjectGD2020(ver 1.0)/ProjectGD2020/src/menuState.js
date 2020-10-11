cd8fa0ed700e9f3dded8cb5676ea9f8631ac1528
Silrein <54448484+Silrein@users.noreply.github.com>
2020-10-11 08:32:50 +0800

    Realigned startbutton
    - menustate

--- ProjectGD2020(ver 1.0)/ProjectGD2020/src/menuState.js
+++ ProjectGD2020(ver 1.0)/ProjectGD2020/src/menuState.js
@@ -3,7 +3,7 @@
         var bg = game.add.tileSprite(0, 0, 800, 600, 'background');
         var gameLabel = game.add.text(250, 80, 'Haunted Mine', { font: '40px Courier', fill: '#fffff' });
         this.bgm = game.add.audio('bgm', 1, true);
-        var startBut = game.add.button(250, game.world.height - 120, 'startButton', this.startButtonAction);
+        var startBut = game.add.button(250, 120, 'startButton', this.startButtonAction);
         this.bgm.play();
     },
     startButtonAction: function startButtonAction() {
